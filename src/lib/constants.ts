f22c6f880a2d21f6efdb3c1b8259d648ff73e0ec
UjwalThakkar <ujwalpotoba02@gmail.com>
2025-06-21 13:35:40 +0530

    changed api url

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,3 +1,3 @@
 export const user_id = '685008f959287c2267edc561'
-export const VITE_API_URL = 'http://139.59.26.29/'
+export const VITE_API_URL = 'http://139.59.26.29/api/v1'
 // export const VITE_API_URL = 'http://localhost:5000/api/v1'
